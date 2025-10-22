19a673dd490360823845bbd43beda05bf8709bdc
VasuAggarwal007 <aggarwa2@ualberta.ca>
2025-10-22 14:38:39 -0600

    Test Cases: Switchactivity, cityconsistency, backbutton

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -3,6 +3,8 @@ package com.example.androiduitesting;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -11,6 +13,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
 
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -34,7 +38,7 @@ public class MainActivityTest {
         // Click on Add City button
         onView(withId(R.id.button_add)).perform(click());
         // Type "Edmonton" in the editText
-        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        onView(withId(R.id.editText_name)).perform(typeText("Edmonton"));
         // Click on Confirm
         onView(withId(R.id.button_confirm)).perform(click());
 
@@ -47,12 +51,12 @@ public class MainActivityTest {
 
         // Add first city to the list
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        onView(withId(R.id.editText_name)).perform(typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
 
         //Add another city to the list
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Vancouver"));
+        onView(withId(R.id.editText_name)).perform(typeText("Vancouver"));
         onView(withId(R.id.button_confirm)).perform(click());
 
         //Clear the list
@@ -66,7 +70,7 @@ public class MainActivityTest {
 
         //Add a city
         onView(withId(R.id.button_add)).perform(click());
-        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
+        onView(withId(R.id.editText_name)).perform(typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
 
         // Check if in the Adapter view (given id of that adapter view), there is a data
@@ -74,6 +78,85 @@ public class MainActivityTest {
         // If this data matches the text we provided then Voila! Our test should pass
         onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list
         )).atPosition(0).check(matches((withText("Edmonton"))));
+
+    }
+
+    /**
+     * Test 1: Check whether the activity is correctly switched from main view to detail view
+     */
+    @Test
+    public void testActivitySwitch(){
+
+        // Add first city to the list
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(typeText("Edmonton"));
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        //Verify if main view if displayed
+        onView(withId(R.id.main_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.detail_view)).check(matches(not(isDisplayed())));
+
+        //Click the city in the list
+        onData(anything())
+                .inAdapterView(withId(R.id.city_list))
+                .atPosition(0)
+                .perform(click());
+
+        //Verify the detail view is displayed and main view is not
+        onView(withId(R.id.main_view)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.detail_view)).check(matches((isDisplayed())));
+
+    }
+
+    /**
+     * Test 2: Check whether the city name stays consistent in the detail view
+     */
+    @Test
+    public void testCityNameConsistency(){
+        String testCity = "Vancouver";
+
+        //Add a city
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(typeText(testCity), closeSoftKeyboard());
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        //Click the city
+        onData(anything())
+                .inAdapterView(withId(R.id.city_list))
+                .atPosition(0)
+                .perform(click());
+
+        //Verify city name
+        onView(withId(R.id.detail_city_name)).check(matches(withText(testCity)));
+
+    }
+
+    /**
+     * Test 3: Checks back button. Pressing back takes you back to main view
+     */
+    @Test
+    public void testBackButton(){
+
+        //Add a city
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(typeText("Edmonton"));
+        onView(withId(R.id.button_confirm)).perform(click());
+
+        //Click the city
+        onData(anything())
+                .inAdapterView(withId(R.id.city_list))
+                .atPosition(0)
+                .perform(click());
+
+        //Verify we are in detail view
+        onView(withId(R.id.detail_view)).check(matches((isDisplayed())));
+
+        //Click back button
+        onView(withId(R.id.button_back)).perform(click());
+
+        //verify we are back to main view
+        onView(withId(R.id.main_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.detail_view)).check(matches(not(isDisplayed())));
 
     }
     
